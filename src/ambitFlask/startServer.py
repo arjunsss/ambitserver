2ce035253aba6c34d5031ad9429cefd39aef97bb
Arjun <arjun@foursquare.com>
2015-09-10 23:37:00 -0700

    Addding audio

--- src/ambitFlask/startServer.py
+++ src/ambitFlask/startServer.py
@@ -1,9 +1,42 @@
-from flask import Flask
-app = Flask(__name__)
+#!/usr/bin/env python
+from flask import Flask, render_template
+from flask.ext.uwsgi_websocket import GeventWebSocket
+import numpy
 
-@app.route('/abc')
-def hello_world():
-    return 'Hello World!'
+app = Flask(__name__)
+ws = GeventWebSocket(app)
+
+@app.route('/audio')
+def audio():
+	return render_template('audio.html')
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@ws.route('/websocket')
+def webSocket(ws):
+   first_message = True
+   total_msg = ""
+   sample_rate = 0
+
+   while True:
+      msg = ws.receive()
+
+      if first_message and msg is not None: # the first message should be the sample rate
+         #sample_rate = getSampleRate(msg)
+         first_message = False
+         continue
+      elif msg is not None:
+         audio_as_int_array = numpy.frombuffer(msg, 'i2')
+         doSomething(audio_as_int_array)
+      else:
+         break
+
+def doSomething(audio_as_int_array):
+	if audio_as_int_array.size > 0:
+		print(audio_as_int_array)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, threads=16, port=8000)
